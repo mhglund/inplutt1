b083dd446decc2a5990565d22f4528126d7ac05d
patrickvirtanen <patrick.virtanen@gmail.com>
2017-03-29 18:38:25 +0200

    Jag lade till en droppMeny som ger en text som feedback när man väljer ett objekt

--- src/InpluppProgram.java
+++ src/InpluppProgram.java
@@ -7,78 +7,96 @@ import java.util.*;
  * inlupp 1 PROG2
  */
 
-	
-public class InpluppProgram extends JFrame implements ActionListener  {
-    //private ArrayList<Vardesak> saker = new ArrayList<>();
-    private ArrayList<Smycke> smycken = new ArrayList<>();
-    private ArrayList<Aktie> aktier = new ArrayList<>();
-    private ArrayList<Apparat> apparater = new ArrayList<>();
-    
-    private JTextField textFalt;
-	private JLabel halsning;
+public class InpluppProgram extends JFrame implements ActionListener {
+	// private ArrayList<Vardesak> saker = new ArrayList<>();
+	private ArrayList<Smycke> smycken = new ArrayList<>();
+	private ArrayList<Aktie> aktier = new ArrayList<>();
+	private ArrayList<Apparat> apparater = new ArrayList<>();
+	private JComboBox<String> box = new JComboBox<>(); // En lista med saker till dropmenyn
 
-    public void fonsterRuta(){
-        setLayout(new BorderLayout());
-        JPanel topPanel = new JPanel();
-        JPanel bottenPanel = new JPanel();
-        JPanel vansterPanel = new JPanel();
-        JPanel hogerPanel = new JPanel();
-        JPanel mittenPanel = new JPanel();
+	private JTextField textFalt;
+	private JLabel label;
 
-        topPanel.add(new JLabel("Värdesaker"));
-        add(topPanel, BorderLayout.NORTH);
+	public void fonsterRuta() {
+		setLayout(new BorderLayout());
+		JPanel topPanel = new JPanel();
+		JPanel bottenPanel = new JPanel();
+		JPanel vansterPanel = new JPanel();
+		JPanel hogerPanel = new JPanel();
+		JPanel mittenPanel = new JPanel();
 
-        bottenPanel.add(textFalt = new JTextField(8));
-        add(bottenPanel, BorderLayout.SOUTH);
+		topPanel.add(new JLabel("Värdesaker"));
+		add(topPanel, BorderLayout.NORTH);
 
-        //center verkar inte funka?? gäller det något spec för den?
-        JButton knapp = new JButton("Hälsa");
-        mittenPanel.add(knapp);
-        add(mittenPanel, BorderLayout.CENTER);
-        knapp.addActionListener(this);
+		bottenPanel.add(textFalt = new JTextField(8));
+		add(bottenPanel, BorderLayout.SOUTH);
 
-        halsning = new JLabel("");
-        add(halsning);
+		// objekt som läggs till i droppmenyn
+		box.addItem("Smycke");
+		box.addItem("Aktie");
+		box.addItem("Apparat");
+		bottenPanel.add(box);
 
+		// center verkar inte funka?? gäller det något spec för den?
+		// Kommer förmodligen att fungera efter vi gjort alla andra borderlayouts.
+		JButton knapp = new JButton("Hälsa");
+		mittenPanel.add(knapp);
+		add(mittenPanel, BorderLayout.CENTER);
+		knapp.addActionListener(this);
 
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(500,600);
-        setVisible(true);
+		label = new JLabel("");
+		add(label);
 
-    }
-	
+		box.addActionListener(this);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(500, 600);
+		setVisible(true);
+
+	}
 
 	public InpluppProgram() {
 		super("Sakregister");
 
 	}
 
-	public void actionPerformed(ActionEvent ave) {
-		String namn = textFalt.getText();
-		halsning.setText("Hej " + namn);
+	public void actionPerformed(ActionEvent ave) { // Jag lade till en lyssnare bara för att se att programmet gav response.
+		if (box.getSelectedIndex() == 0) {
+			label.setText("Du har valt smycke");
+		} else if (box.getSelectedIndex() == 1) {
+			label.setText("Du har valt aktie");
+		} else if (box.getSelectedIndex() == 2) {
+			label.setText("Du har valt apparat");
+		}
 	}
 
+	private void setUp() {
+		Smycke s1 = new Smycke("halsband", 2, true);
+		smycken.add(s1);
+		Aktie ak1 = new Aktie("Ericsson", 4, 0.25);
+		aktier.add(ak1);
+		Apparat ap1 = new Apparat("teve", 3000.00, 5);
+		apparater.add(ap1);
 
-    private void setUp() {
-        Smycke s1 = new Smycke("halsband", 2, true);
-        smycken.add(s1);
-        Aktie ak1 = new Aktie("Ericsson", 4, 0.25);
-        aktier.add(ak1);
-        Apparat ap1 = new Apparat("teve", 3000.00, 5);
-        apparater.add(ap1);
+		System.out.println(smycken.get(0));
+		System.out.println(aktier.get(0));
+		System.out.println(apparater.get(0));
+	}
 
+	public static void main(String args[]) {
+		InpluppProgram program = new InpluppProgram();
 
-        System.out.println(smycken.get(0));
-        System.out.println(aktier.get(0));
-        System.out.println(apparater.get(0));
-    }
+		program.setUp();
+		program.fonsterRuta();
 
-    public static void main(String args[]) {
-        InpluppProgram program = new InpluppProgram();
+	}
 
-        program.setUp();
-        program.fonsterRuta();
+	static {
+		Font f = new Font("Dialog", Font.BOLD, 20);
+		String[] comps = { "Button", "Label", "RadioButton", "CheckBox", "ToggleButton", "TextArea", "TextField",
+				"Menu", "MenuItem", "FileChooser", "Dialog", "OptionPane" };
+		for (String s : comps) {
+			UIManager.put(s + ".font", f);
+		}
 
-
-    }
+	}
 }
